d2b01a2308a2544d65166ed4e1cab0c261a149e7
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-10-08 15:02:38 +0200

    feat: add input prompt dialog

--- src/_macos.c.v
+++ src/_macos.c.v
@@ -2,20 +2,28 @@ module dialog
 
 import webview
 
+fn dialog__message(message string, opts MessageOptions) bool {
+	w := webview.create()
+	defer {
+		w.destroy()
+	}
+	return dialog_c__message(message, opts)
+}
+
+fn dialog__prompt(message string, opts PromptOptions) ?string {
+	w := webview.create()
+	defer {
+		w.destroy()
+	}
+	return dialog_c__prompt(message, opts)
+}
+
 fn dialog__file_dialog() ?string {
 	w := webview.create()
 	defer {
 		w.destroy()
 	}
 	return dialog_c__file_dialog()
-}
-
-fn dialog__message(text string, opts MessageOptions) bool {
-	w := webview.create()
-	defer {
-		w.destroy()
-	}
-	return dialog_c__message(text, opts)
 }
 
 fn dialog__color_picker(opts ColorPickerOptions) ?Color {

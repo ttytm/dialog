5eca968b12464f7cfeb69d4a7b432bcd44af52e5
Turiiya <34311583+ttytm@users.noreply.github.com>
2023-10-08 22:31:12 +0200

    feat: add `open_file`, `open_dir` and `save_file` (#2)

--- src/_default.c.v
+++ src/_default.c.v
@@ -8,8 +8,21 @@ fn dialog__prompt(message string, opts PromptOptions) ?string {
 	return dialog_c__prompt(message, opts)
 }
 
+[deprecated]
 fn dialog__file_dialog() ?string {
-	return dialog_c__file_dialog()
+	return unsafe { dialog_c__file_dialog(.open, nil, nil) }
+}
+
+fn dialog__open_file(opts FileOpenOptions) ?string {
+	return dialog_c__file_dialog(.open, &char(opts.path.str), unsafe { nil })
+}
+
+fn dialog__open_dir(opts FileOpenOptions) ?string {
+	return dialog_c__file_dialog(.open_dir, &char(opts.path.str), unsafe { nil })
+}
+
+fn dialog__save_file(opts FileSaveOptions) ?string {
+	return dialog_c__file_dialog(.save, &char(opts.path.str), &char(opts.filename.str))
 }
 
 fn dialog__color_picker(opts ColorPickerOptions) ?Color {
--- src/_macos.c.v
+++ src/_macos.c.v
@@ -18,12 +18,37 @@ fn dialog__prompt(message string, opts PromptOptions) ?string {
 	return dialog_c__prompt(message, opts)
 }
 
+[deprecated]
 fn dialog__file_dialog() ?string {
 	w := webview.create()
 	defer {
 		w.destroy()
 	}
-	return dialog_c__file_dialog()
+	return unsafe { dialog_c__file_dialog(.open, nil, nil) }
+}
+
+fn dialog__open_file(opts FileOpenOptions) ?string {
+	w := webview.create()
+	defer {
+		w.destroy()
+	}
+	return dialog_c__file_dialog(.open, &char(opts.path.str), unsafe { nil })
+}
+
+fn dialog__open_dir(opts FileOpenOptions) ?string {
+	w := webview.create()
+	defer {
+		w.destroy()
+	}
+	return dialog_c__file_dialog(.open_dir, &char(opts.path.str), unsafe { nil })
+}
+
+fn dialog__save_file(opts FileSaveOptions) ?string {
+	w := webview.create()
+	defer {
+		w.destroy()
+	}
+	return dialog_c__file_dialog(.save, &char(opts.path.str), &char(opts.filename.str))
 }
 
 fn dialog__color_picker(opts ColorPickerOptions) ?Color {
